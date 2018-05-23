1e0d848352a67fd1eca13d20d5ed80fabe1fa4e0
xu ding <thedilab@gmail.com>
2018-05-23 10:23:59 +0800

    guard data

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -3,6 +3,8 @@
 namespace Omnipay\Eghl\Message;
 
 
+use Omnipay\Common\Exception\InvalidRequestException;
+
 class PurchaseRequest extends AbstractRequest
 {
     public function sendData($data)
@@ -27,7 +29,7 @@ class PurchaseRequest extends AbstractRequest
 
         $amount = number_format($this->getAmount(), 2, '.', '');
 
-        return [
+        $data = [
             'TransactionType' => 'SALE',
             'PymtMethod' => 'ANY',
             'ServiceID' => $this->getMerchantId(),
@@ -45,6 +47,68 @@ class PurchaseRequest extends AbstractRequest
             'CustPhone' => $this->getCard()->getPhone(),
             'PageTimeout' => 600
         ];
+
+        $this->guardGatewayData($data);
+
+        return $data;
+    }
+
+    private function guardGatewayData($data)
+    {
+        if (strlen($data['PaymentID']) > 20) {
+            throw new InvalidRequestException(
+                sprintf('Invalid PaymentID length: %s, max length is 20', strlen($data['PaymentID']))
+            );
+        }
+
+        if (strlen($data['OrderNumber']) > 20) {
+            throw new InvalidRequestException(
+                sprintf('Invalid OrderNumber length: %s, max length is 20', strlen($data['OrderNumber']))
+            );
+        }
+
+        if (strlen($data['PaymentDesc']) > 20) {
+            throw new InvalidRequestException(
+                sprintf('Invalid PaymentDesc length: %s, max length is 100', strlen($data['PaymentDesc']))
+            );
+        }
+
+        if (strlen($data['MerchantReturnURL']) > 255) {
+            throw new InvalidRequestException(
+                sprintf('Invalid MerchantReturnURL length: %s, max length is 255', strlen($data['MerchantReturnURL']))
+            );
+        }
+
+        if (strlen($data['MerchantCallBackURL']) > 255) {
+            throw new InvalidRequestException(
+                sprintf('Invalid MerchantCallBackURL length: %s, max length is 255', strlen($data['MerchantCallBackURL']))
+            );
+        }
+
+        if (strlen($data['CustIP']) > 20) {
+            throw new InvalidRequestException(
+                sprintf('Invalid CustIP length: %s, max length is 20', strlen($data['CustIP']))
+            );
+        }
+
+        if (strlen($data['CustName']) > 50) {
+            throw new InvalidRequestException(
+                sprintf('Invalid CustName length: %s, max length is 50', strlen($data['CustName']))
+            );
+        }
+
+        if (strlen($data['CustEmail']) > 60) {
+            throw new InvalidRequestException(
+                sprintf('Invalid CustEmail length: %s, max length is 60', strlen($data['CustEmail']))
+            );
+        }
+
+        if (strlen($data['CustPhone']) > 25) {
+            throw new InvalidRequestException(
+                sprintf('Invalid CustPhone length: %s, max length is 25', strlen($data['CustPhone']))
+            );
+        }
+
     }
 
     private function safeUrl($url)
